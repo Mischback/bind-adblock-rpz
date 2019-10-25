f0309657a76bc66435655651c1fdb6a829da6b8f
Mischback <dev@mischback.de>
2019-10-25 22:25:04 +0200

    Created HttpBlocklistProvider class

--- rpz_updater.py
+++ rpz_updater.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+# Python imports
+import requests
+
+class BlocklistProviderException(Exception):
+    pass
+
+class BlocklistProviderExceptionNotImplemented(BlocklistProviderException):
+    pass
+
 class BlocklistProvider(object):
-
-    class BlocklistProviderException(Exception):
-        pass
-
-    class BlocklistProviderExceptionNotImplemented(BlocklistProviderException):
-        pass
 
     def __init__(self):
         """Creates an instance of BlocklistProvider.
@@ -34,3 +37,33 @@ class BlocklistProvider(object):
         """This method returns the converted blocklist."""
 
         return self.blocklist
+
+class HttpBlocklistProvider(BlocklistProvider):
+
+    def __init__(self, url):
+
+        # store the URL in the object
+        self.url = url
+
+        # call parent constructor
+        super().__init__()
+
+    def fetch(self):
+
+        headers = {
+            'User-Agent': 'bind-adblock-rpz'
+        }
+
+        try:
+            # TODO: Make timeout configurable!
+            response = requests.get(self.url, headers=headers, timeout=10)
+
+            if response.status_code == 200:
+                return response.text
+        except requests.exceptions.RequestException as e:
+            print(e)    # TODO: fix this! Should be logged to logfile!
+            return None
+
+
+if __name__ == '__main__':
+    foo = HttpBlocklistProvider('http://mischback.de')
